f668f1cdf2852b2bffdd6f536ff37b7e9ccf39e6
leacohen <Lea.cohen.sabban@gmail.com>
2016-08-16 19:18:58 +0300

    added system suggestion page

--- Web Site Managment/app/client/system-suggestion/system-suggestion.controller.js
+++ Web Site Managment/app/client/system-suggestion/system-suggestion.controller.js
@@ -3,15 +3,47 @@ angular.module('orledor')
 
 		refreshMedia();
 
+		$scope.mediaTypeToMdi = function(type) {
+			if (type === 'Song') {
+				return 'mdi-music-note'
+			}
+
+			if (type === 'Movie') {
+				return 'mdi-movie';
+			}
+
+			return 'mdi-account';
+		}
+
+		$scope.openLink = function(linkUrl) {
+			window.open(linkUrl, '_blank').focus();
+		}
+
 		function refreshMedia() {
 			$scope.medias = [];
+			$scope.isWaitingForMedia = true;
 			return firebase.child('multimedia').once('value')
 				.then(function(mult) {
 					mult.forEach(function(currentMultemdia) {
 						$scope.medias.push(currentMultemdia.val())
 					});
-					$scope.$apply();
 				})
+				.then(function() {
+					$scope.medias.sort(mediaSortAlgoritm);
+				})
+				.then(function() {
+					$scope.isWaitingForMedia = false;
+					$scope.$apply();
+				});
 		};
 
+		function mediaSortAlgoritm(a, b) {
+			if (a._publishDate > b._publishDate) {
+				return -1;
+			} else if (a._publishDate < b._publishDate) {
+				return 1;
+			}
+
+			return 0;
+		}
 	});
