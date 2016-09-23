b0464731b25a4b6e088e7452c93d67337770cd72
leacohen <Lea.cohen.sabban@gmail.com>
2016-09-23 12:46:14 +0300

    fixed adding favorite link style & validation

--- Web Site Managment/app/client/system-suggestion/system-suggestion.controller.js
+++ Web Site Managment/app/client/system-suggestion/system-suggestion.controller.js
@@ -1,178 +1,166 @@
 angular.module('orledor')
-	.controller('systemSuggestionController', function($scope, firebase, loggedUser, $q, mediaSort) {
+    .controller('systemSuggestionController', function($scope, firebase, loggedUser, $q, mediaSort) {
 
-		$scope.isWaitingForMedia = true;
-		initMediaTypes();
+        $scope.isWaitingForMedia = true;
+        initMediaTypes();
 
-		var promise = $q.resolve();
-		if (!loggedUser.getUser()) {
-			// TODO: Temp!!! autologin to a@aa
-			promise = firebase.child("users").child("leacohen").once("value")
-				.then(function(user) {
-					var userValue = user.val();
-					loggedUser.setUser(userValue);
-				})
-		}
+        $scope.user = loggedUser.getUser();
+        $scope.user.likes = $scope.user.likes || {};
 
-		promise.then(function() {
-			$scope.user = loggedUser.getUser();
-			$scope.user.likes = $scope.user.likes || {};
+        refreshMedia();
 
-			refreshMedia();
-		})
+        $scope.mediaTypeToMdi = function(type) {
+            if (type === 'Song') {
+                return 'mdi-music-note'
+            }
 
-		$scope.mediaTypeToMdi = function(type) {
-			if (type === 'Song') {
-				return 'mdi-music-note'
-			}
+            if (type === 'Movie') {
+                return 'mdi-movie';
+            }
 
-			if (type === 'Movie') {
-				return 'mdi-movie';
-			}
+            return 'mdi-account';
+        }
 
-			return 'mdi-account';
-		}
+        $scope.openLink = function(linkUrl) {
+            window.open(linkUrl, '_blank').focus();
+        }
 
-		$scope.openLink = function(linkUrl) {
-			window.open(linkUrl, '_blank').focus();
-		}
+        $scope.like = function(media) {
+            var mediaName = media._name;
 
-		$scope.like = function(media) {
-			var mediaName = media._name;
+            if (!media.likes) {
+                media.likes = 0;
+            }
 
-			if(!media.likes) {
-				media.likes = 0;
-			}
+            media.likes++;
 
-			media.likes++;
+            return preformVote(media, 1)
+                .then(function() {
+                    return updateMedia(angular.copy(media));
+                })
+                .catch(function(err) {
+                    console.log(err);
+                });
+        }
 
-			return preformVote(media, 1)
-				.then(function() {
-					return updateMedia(angular.copy(media));
-				})
-				.catch(function (err) {
-					console.log(err);
-				});
-		}
+        $scope.dislike = function(media) {
+            var mediaName = media._name;
 
-		$scope.dislike = function(media) {
-			var mediaName = media._name;
+            if (!media.likes) {
+                media.likes = 0;
+            }
 
-			if (!media.likes) {
-				media.likes = 0;
-			}
+            media.likes--;
 
-			media.likes--;
+            return preformVote(media, -1)
+                .then(function() {
+                    return updateMedia(angular.copy(media));
+                })
+                .catch(function(err) {
+                    console.log(err);
+                });
+        }
 
-			return preformVote(media, -1)
-				.then(function() {
-					return updateMedia(angular.copy(media));
-				})
-				.catch(function (err) {
-					console.log(err);
-				});
-		}
+        function preformVote(media, value) {
+            if ($scope.user.likes[media._name] === value) {
+                return unvote(media._name);
+            }
 
-		function preformVote(media, value) {
-			if ($scope.user.likes[media._name] === value) {
-				return unvote(media._name);
-			}
-
-			return vote(media._name, value);
-		}
+            return vote(media._name, value);
+        }
 
 
-		function refreshMedia() {
-			$scope.medias = [];
-			$scope.isWaitingForMedia = true;
-			return firebase.child('multimedia').once('value')
-				.then(function(mult) {
-					mult.forEach(function(currentMultemdia) {
-						var newMedia = currentMultemdia.val();
-						$scope.medias.push(newMedia);
-					});
-				})
-				.then(function() {					
-					$scope.medias = mediaSort.sortSystemSuggest($scope.medias);
-				})
-				.then(function() {
-					$scope.isWaitingForMedia = false;
-					$scope.$apply();
-				});
-		};
+        function refreshMedia() {
+            $scope.medias = [];
+            $scope.isWaitingForMedia = true;
+            return firebase.child('multimedia').once('value')
+                .then(function(mult) {
+                    mult.forEach(function(currentMultemdia) {
+                        var newMedia = currentMultemdia.val();
+                        $scope.medias.push(newMedia);
+                    });
+                })
+                .then(function() {
+                    $scope.medias = mediaSort.sortSystemSuggest($scope.medias);
+                })
+                .then(function() {
+                    $scope.isWaitingForMedia = false;
+                    $scope.$apply();
+                });
+        };
 
-		function updateMedia(media) {
-			return firebase.child('multimedia')
-				.child(media._name)
-				.update(media);
-		}
+        function updateMedia(media) {
+            return firebase.child('multimedia')
+                .child(media._name)
+                .update(media);
+        }
 
-		function vote(mediaName, voteValue) {
-			$scope.user.likes[mediaName] = voteValue;
+        function vote(mediaName, voteValue) {
+            $scope.user.likes[mediaName] = voteValue;
 
-			return updateUser();
-		}
+            return updateUser();
+        }
 
-		function unvote(mediaName) {
-			delete $scope.user.likes[mediaName];
+        function unvote(mediaName) {
+            delete $scope.user.likes[mediaName];
 
-			return updateUser();
-		}
+            return updateUser();
+        }
 
-		function updateUser() {
-			return firebase.child('users')
-				.child($scope.user._userName)
-				.update($scope.user);
-		}
+        function updateUser() {
+            return firebase.child('users')
+                .child($scope.user._userName)
+                .update($scope.user);
+        }
 
-		function getUserVote(mediaName) {
-			var user = loggedUser.getUser();
+        function getUserVote(mediaName) {
+            var user = loggedUser.getUser();
 
-			if (!user || !user.likes) {
-				return 0;
-			}
+            if (!user || !user.likes) {
+                return 0;
+            }
 
-			var userVote = user.likes[mediaName];
-			if (!userVote) {
-				return 0;
-			}
+            var userVote = user.likes[mediaName];
+            if (!userVote) {
+                return 0;
+            }
 
-			return userVote;
-		}
+            return userVote;
+        }
 
-		function mediaSortAlgoritm(a, b) {
-			var aLike = $scope.user.likes[a._name] || 0;
-			var bLike = $scope.user.likes[b._name] || 0;
+        function mediaSortAlgoritm(a, b) {
+            var aLike = $scope.user.likes[a._name] || 0;
+            var bLike = $scope.user.likes[b._name] || 0;
 
-			if(aLike > bLike) {
-				return -1;
-			}
+            if (aLike > bLike) {
+                return -1;
+            }
 
-			if (aLike < bLike) {
-				return 1;
-			}
+            if (aLike < bLike) {
+                return 1;
+            }
 
-			if (a.likes > b.likes) {
-				return -1;
-			}
+            if (a.likes > b.likes) {
+                return -1;
+            }
 
-			if (a.likes < b.likes) {
-				return 1;
-			}
+            if (a.likes < b.likes) {
+                return 1;
+            }
 
-			return 0;
-		}
+            return 0;
+        }
 
-		function initMediaTypes() {
-			$scope.mediaTypes = [{
-				name: '',
-				displayName: 'הצג הכול'
-			}, {
-				name: 'Song',
-				displayName: 'מוזיקה'
-			}, {
-				name: 'Movie',
-				displayName: 'סרטים'
-			}];
-		}
-	});
+        function initMediaTypes() {
+            $scope.mediaTypes = [{
+                name: '',
+                displayName: 'הצג הכול'
+            }, {
+                name: 'Song',
+                displayName: 'מוזיקה'
+            }, {
+                name: 'Movie',
+                displayName: 'סרטים'
+            }];
+        }
+    });
