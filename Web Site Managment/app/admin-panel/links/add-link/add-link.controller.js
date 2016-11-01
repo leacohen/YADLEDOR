fba0bf1c68a8ad2efe9d4ad17e53e122bd3aa590
leacohen <Lea.cohen.sabban@gmail.com>
2016-11-01 22:49:13 +0200

    now when adding 'Other' media to the media links, the publish date field is hidden

--- Web Site Managment/app/admin-panel/links/add-link/add-link.controller.js
+++ Web Site Managment/app/admin-panel/links/add-link/add-link.controller.js
@@ -1,10 +1,44 @@
-angular.module('orledor').controller('addLinkController', function($scope, $mdDialog, firebase) {
-  $scope.saveLink = function() {
-    // Temp...should talk about it..
-    $scope.link._publishDate = $scope.publishDate.toISOString();
-    $scope.link._language = "Hebrew";
+angular.module('orledor').controller('addLinkController', function($scope, $mdDialog, $q, $mdToast, firebase) {
+	$scope.link = {};
 
-    firebase.child('multimedia').child($scope.link._name).set($scope.link);
-    $mdDialog.hide();
-  };
+	$scope.saveLink = function() {
+		return ensureFields()
+			.then(function() {
+				if ($scope.link._type !== 'Other') {
+					$scope.link._publishDate = $scope.publishDate.toISOString();
+				}
+
+				// TODO: shold impl langs..
+				$scope.link._language = "Hebrew";
+
+				return firebase.child('multimedia').child($scope.link._name).set($scope.link);
+			})
+			.then($mdDialog.hide)
+			.catch(function(error) {
+				if (error) {
+					$mdToast.showSimple(error);
+				}
+
+				return $q.reject(error);
+			});
+
+	};
+
+	function ensureFields() {
+		if (!$scope.link._name) {
+			return $q.reject('מדיה חייבת להכיל שם');
+		}
+
+		if (!$scope.publishDate && $scope.link._type !== 'Other') {
+			return $q.reject('מדיה חייבת להכיל תאריך הוצאה');
+		}
+
+		if (!$scope.link._link) {
+			return $q.reject('מדיה חייבת להכיל קישור');
+		}
+
+		return $q.resolve();
+	}
+
+
 });
