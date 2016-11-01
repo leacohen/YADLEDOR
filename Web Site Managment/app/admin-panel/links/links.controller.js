b7e7576105fdd9ea521a6edca28fbd4130efb03b
leacohen <Lea.cohen.sabban@gmail.com>
2016-11-01 22:16:53 +0200

    added warning alert before delete media

--- Web Site Managment/app/admin-panel/links/links.controller.js
+++ Web Site Managment/app/admin-panel/links/links.controller.js
@@ -4,23 +4,38 @@ angular.module('orledor').controller('linksController', function($scope, $mdDial
 	refreshMedia();
 
 	$scope.createLink = function(ev) {
-		$mdDialog.show({
-			controller: 'addLinkController',
-			templateUrl: 'app/admin-panel/links/add-link/add-link.html',
-			targetEvent: ev,
-			clickOutsideToClose: true,
-		})
-		.then(refreshMedia);
+		return $mdDialog.show({
+				controller: 'addLinkController',
+				templateUrl: 'app/admin-panel/links/add-link/add-link.html',
+				targetEvent: ev,
+				clickOutsideToClose: true,
+			})
+			.then(refreshMedia);
 	};
 
-	$scope.delete = function(mediaName) {
-		firebase.child('multimedia').child(mediaName).remove();
-		refreshMedia();
+	$scope.delete = function(event, mediaName) {
+		return warningDeleteAlert(event, mediaName)
+			.then(function() {
+				return firebase.child('multimedia').child(mediaName).remove();
+			})
+			.then(function() {
+				return refreshMedia();
+			})
 	};
+
+	function warningDeleteAlert(event, mediaName) {
+		return $mdDialog.show(
+			$mdDialog.confirm()
+			.clickOutsideToClose(true)
+			.textContent('האם אתה בטוח שברצונך למחוק את המדיה ' + mediaName + '?')
+			.ok('המשך')
+			.cancel('בטל')
+			.targetEvent(event));
+	}
 
 	function refreshMedia() {
 		$scope.links = [];
-		firebase.child('multimedia').once('value')
+		return firebase.child('multimedia').once('value')
 			.then(function(mult) {
 				mult.forEach(function(currentMultemdia) {
 					$scope.links.push(currentMultemdia.val())
